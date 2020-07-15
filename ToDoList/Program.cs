00f311af8c62e8ff631c1891968d70e5804d8c6e
Tristan Emmerson <tristan@stickerslug.com>
2020-07-15 08:52:49 -0700

    update project files.

---
+++ ToDoList/Program.cs
@@ -0,0 +1,16 @@
+using ToDoList;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoList
+{
+  class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
